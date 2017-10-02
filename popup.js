b6d74f5ddb28597eac441f8312ca21c44df603c7
Rupesh Dabbir <rdabbir@paypal.com>
2017-10-02 11:33:33 -0700

    First commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    alert("Something amazing is coming out soon! Stay tuned!");
+    
+  }, false);
+}, false);
